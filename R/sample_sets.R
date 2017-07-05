7b7c4da60ed9c6da2ff4daa4d121a10d7dc52028
Cédric Mondy <cedric.mondy@gmail.com>
2017-07-05 16:21:02 +0200

    function to divide the original data set in three: training, calibration and test

---
+++ R/sample_sets.R
@@ -0,0 +1,33 @@
+#' @importFrom dplyr '%>%'
+sample_sets <- function(x,
+                        prop      = c(train     = 0.7,
+                                      calibrate = 0.15,
+                                      test      = 0.15)) {
+
+  x_id <- 1:length(x)
+
+  calibrate_id <- sample(x       = x_id,
+                         size    = prop[2] * length(x),
+                         replace = FALSE)
+
+  if (length(prop) > 2) {
+    test_id <- sample(x       = x_id[-calibrate_id],
+                      size    = prop[3] * length(x),
+                      replace = FALSE)
+  } else {
+    test_id <- NULL
+  }
+
+
+  train_id <- x_id[-c(calibrate_id, test_id)]
+
+  x_summary <- data.frame(x                = x,
+                          sample           = NA,
+                          stringsAsFactors = FALSE)
+
+  x_summary$sample[train_id]     <- "train"
+  x_summary$sample[calibrate_id] <- "calibrate"
+  x_summary$sample[test_id]      <- "test"
+
+  return(x_summary)
+}
